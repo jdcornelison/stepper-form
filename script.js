13e6a7b3d04e12c2c09d796e266224bf06b2bddc
jdcornelison <jdcornelison@gmail.com>
2020-02-23 16:26:59 -0600

    More spelling ... inventions?

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let scrollPos = 0
 
 function setNavText () {
   if (scrollPos >= (stepWidth * (stepsArray.length -1))) {
-    // If we're on the last step, set next nav text to a green checkmark
+    // If we're on the last step, set next nav text to a green check mark
     document.querySelector('.step-nav.next span').style.color = 'green'
     document.querySelector('.step-nav.next span').textContent = '✔'
   } else if (scrollPos === 0) {
@@ -13,14 +13,14 @@ function setNavText () {
     document.querySelector('.step-nav.prev span').textContent = ''
     document.querySelector('.step-nav.prev').classList.remove('nav-enabled')
   } else {
-    // This seems redundent to do ever time we're not on the first or last
+    // This seems redundant to do ever time we're not on the first or last
     // step of the form, but my brain hurts. I'm not as good at CSS & Flex
     // as I thought I was...
 
     // Double check that the hover effect and text is added to the prev nav
     document.querySelector('.step-nav.prev span').textContent = '«'
     document.querySelector('.step-nav.prev').classList.add('nav-enabled')
-    // Double chek that the next nav text is correct and set the proper color
+    // Double check that the next nav text is correct and set the proper color
     document.querySelector('.step-nav.next span').textContent = '»'
     document.querySelector('.step-nav.next span').style.color = 'rgba(255, 255, 255, 0.75)'
   }
